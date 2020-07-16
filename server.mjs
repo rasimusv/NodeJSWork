24d6a6628e3bc19d39c1379599ac06c85a6de025
rasimusv <rasimusv@gmail.com>
2020-07-16 22:07:25 +0300

    Initial commit
    Init

---
+++ server.mjs
@@ -0,0 +1,10 @@
+import 'bootstrap';
+// используем библиотеку express
+const express = require('express');
+// создаем объект express
+const app = express();
+// говорим, что мы раздаем папку public
+app.use(express.static('public'));
+// говорим, что запускаемся на порту 80
+app.listen(80);
+console.log("Server started at 80");
